a09d9a01ca453dec8ca29480c959681353137f5c
Minh Ton <37860569+minh-ton@users.noreply.github.com>
2026-02-23 22:24:12 +0700

    Refactor app main; use latest changes from truefox

--- Reynard/main.swift
+++ Reynard/main.swift
@@ -9,25 +9,4 @@ import Foundation
 import GeckoWrapper
 import UIKit
 
-let defaultArgs = CommandLine.arguments
-let debugArgs = [
-    // Debug logging
-    "-pref", "geckoview.logging=Debug",
-]
-let allArgs = defaultArgs + debugArgs
-
-let argsCount = Int32(allArgs.count)
-let argv = UnsafeMutablePointer<UnsafeMutablePointer<Int8>?>.allocate(capacity: Int(argsCount + 1))
-for (index, arg) in allArgs.enumerated() {
-    argv[index] = strdup(arg)
-}
-
-argv[Int(argsCount)] = nil
-defer {
-    for i in 0..<Int(argsCount) {
-        free(argv[i])
-    }
-    argv.deallocate()
-}
-
-GeckoRuntime.main(argc: argsCount, argv: argv)
+GeckoRuntime.main(argc: CommandLine.argc, argv: CommandLine.unsafeArgv)
